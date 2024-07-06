6da4344ca58901ef752154ca8997744cafb39d77
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-07-06 14:14:26 +0200

    day 1 completed

--- day1.py
+++ day1.py
@@ -23,6 +23,16 @@ def compute_part_one(file_name: str) -> int:
     return 0
 
 
+def compute_part_two(file_name: str) -> int:
+    numbers = read_input_file(file_name)
+    for n1, n2, n3 in combinations(numbers, 3):
+        if n1 + n2 + n3 == 2020:
+            print(n1, n2, n3, n1 * n2 * n3)
+            return n1 * n2 * n3
+
+    return 0
+
+
 if __name__ == '__main__':
     print(f"Part I: {compute_part_one('input/input1.txt')}")
-    # print(f"Part II: {compute_part_two('input/input1.txt')}")
+    print(f"Part II: {compute_part_two('input/input1.txt')}")
