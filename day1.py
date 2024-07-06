fcc4b6b66c964727fca3d47bccd797dde16a91e2
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-07-06 11:06:34 +0200

    initial commit

---
+++ day1.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+def read_input_file(file_name: str) -> list:
+    with open(file_name) as f:
+        content = f.read().splitlines()
+
+    numbers = []
+    for line in content:
+        numbers.append(int(line))
+
+    print(numbers)
+    return numbers
+
+
+def compute_part_one(file_name: str) -> int:
+    numbers = read_input_file(file_name)
+    for n1, n2 in combinations(numbers, 2):
+        if n1 + n2 == 2020:
+            print(n1, n2, n1 * n2)
+            return n1 * n2
+
+    return 0
+
+
+if __name__ == '__main__':
+    print(f"Part I: {compute_part_one('input/input1.txt')}")
+    # print(f"Part II: {compute_part_two('input/input1.txt')}")
