419ba632f28a9cba0ff42b740de39a94237966fc
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-07-20 11:21:33 +0200

    day 6 completed

--- day6.py
+++ day6.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 
 def read_input_file(file_name: str) -> list:
@@ -18,6 +19,17 @@ def count_answers(answers: list) -> int:
     return len(answer_set)
 
 
+def count_answers_two(answers: list) -> int:
+    pattern = "[a-z]"
+    answer_set = set(string.ascii_lowercase)
+    for answer in answers:
+        letters = re.findall(pattern, answer)
+        set2 = set(letters)
+        answer_set = answer_set.intersection(set2)
+
+    return len(answer_set)
+
+
 def compute_part_one(file_name: str) -> int:
     inputs = read_input_file(file_name)
     sum_of_unique_answers = 0
@@ -31,7 +43,13 @@ def compute_part_one(file_name: str) -> int:
 
 def compute_part_two(file_name: str) -> int:
     inputs = read_input_file(file_name)
-    return 0
+    sum_of_unique_answers = 0
+    for line in inputs:
+        line = line.splitlines()
+        sum_of_unique_answers += count_answers_two(line)
+
+    print(f'{sum_of_unique_answers= }')
+    return sum_of_unique_answers
 
 
 if __name__ == '__main__':
