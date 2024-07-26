92553d86faac93c768a89e9c5e5ea12c931d13cd
Lex van der Stoep <lex.vanderstoep@imc.com>
2024-07-26 23:39:53 +0200

    Add tests
    Let's add automated tests to check that our solutions our correct
    against sample input.

--- day8.py
+++ day8.py
@@ -86,5 +86,5 @@ def compute_part_two(file_name: str) -> int:
 
 
 if __name__ == '__main__':
-    print(f"Part I: {compute_part_one('input/input8.txt')}")
-    print(f"Part II: {compute_part_two('input/input8.txt')}")
+    print(f"Part I: {compute_part_one('test/input/input8.txt')}")
+    print(f"Part II: {compute_part_two('test/input/input8.txt')}")
