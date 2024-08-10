f42c0027bdc51be226a97836d33ea53b7c6611b9
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-08-10 21:45:38 +0200

    day 17 completed

--- day17.py
+++ day17.py
@@ -8,38 +8,47 @@ def read_input_file(file_name: str) -> list:
     return content
 
 
-def transform_cubes_to_set(grid: list) -> set:
+def transform_cubes_to_set(grid: list, part: int) -> set:
     cube_set = set()
     for i, line in enumerate(grid):
         for j, letter in enumerate(line):
             if letter == '#':
-                cube_set.add((i, j, 0))
+                if part == 1:
+                    cube_set.add((i, j, 0))
+                else:
+                    cube_set.add((i, j, 0, 0))
+
     return cube_set
 
 
-def find_min_max(cubes: set) -> (int, int, int, int, int, int):
+def find_min_max(cubes: set, part: int):
     x_min, x_max = min(cubes, key=lambda item: item[0])[0], max(cubes, key=lambda item: item[0])[0]
     y_min, y_max = min(cubes, key=lambda item: item[1])[1], max(cubes, key=lambda item: item[1])[1]
     z_min, z_max = min(cubes, key=lambda item: item[2])[2], max(cubes, key=lambda item: item[2])[2]
+    if part == 2:
+        w_min, w_max = min(cubes, key=lambda item: item[3])[3], max(cubes, key=lambda item: item[3])[3]
+        return x_min, x_max, y_min, y_max, z_min, z_max, w_min, w_max
+
     return x_min, x_max, y_min, y_max, z_min, z_max
 
 
-def apply_rules_to_cube(cubes: set) -> set:
+def apply_rules_to_cube_3d(cubes: set) -> set:
+    part = 1
     new_cubes = set()
-    x_min, x_max, y_min, y_max, z_min, z_max = find_min_max(cubes)
+    x_min, x_max, y_min, y_max, z_min, z_max = find_min_max(cubes, part)
     for x in range(x_min - 1, x_max + 2):
         for y in range(y_min - 1, y_max + 2):
             for z in range(z_min - 1, z_max + 2):
                 if (x, y, z) in cubes:
                     number_of_neighbours = 0
-                    for dx, dy, dz in neighbours():
+                    for dx, dy, dz in neighbours(part):
                         if (x + dx, y + dy, z + dz) in cubes:
                             number_of_neighbours += 1
                     if 2 <= number_of_neighbours <= 3:
                         new_cubes.add((x, y, z))
                 else:
                     number_of_neighbours = 0
-                    for dx, dy, dz in neighbours():
+                    for dx, dy, dz in neighbours(part):
                         if (x + dx, y + dy, z + dz) in cubes:
                             number_of_neighbours += 1
                     if number_of_neighbours == 3:
@@ -48,25 +57,61 @@ def apply_rules_to_cube(cubes: set) -> set:
     return new_cubes
 
 
-def neighbours() -> list:
-    digits = [-1, 0, 1]
-    combinations = list(itertools.product(digits, repeat=3))
-    combinations.remove((0, 0, 0))
+def apply_rules_to_cube_4d(cubes: set) -> set:
+    part = 2
+    new_cubes = set()
+    x_min, x_max, y_min, y_max, z_min, z_max, w_min, w_max = find_min_max(cubes, part)
+    for x in range(x_min - 1, x_max + 2):
+        for y in range(y_min - 1, y_max + 2):
+            for z in range(z_min - 1, z_max + 2):
+                for w in range(w_min -1, w_max + 2):
+                    if (x, y, z, w) in cubes:
+                        number_of_neighbours = 0
+                        for dx, dy, dz, dw in neighbours(part):
+                            if (x + dx, y + dy, z + dz, w + dw) in cubes:
+                                number_of_neighbours += 1
+                        if 2 <= number_of_neighbours <= 3:
+                            new_cubes.add((x, y, z, w))
+                    else:
+                        number_of_neighbours = 0
+                        for dx, dy, dz, dw in neighbours(part):
+                            if (x + dx, y + dy, z + dz, w + dw) in cubes:
+                                number_of_neighbours += 1
+                        if number_of_neighbours == 3:
+                            new_cubes.add((x, y, z, w))
+
+    return new_cubes
+
+
+def neighbours(part: int) -> list:
+    if part == 1:
+        digits = [-1, 0, 1]
+        combinations = list(itertools.product(digits, repeat=3))
+        combinations.remove((0, 0, 0))
+    else:
+        digits = [-1, 0, 1]
+        combinations = list(itertools.product(digits, repeat=4))
+        combinations.remove((0, 0, 0, 0))
+
     return combinations
 
 
 def compute_part_one(file_name: str) -> int:
+    part = 1
     cubes = read_input_file(file_name)
-    cubes = transform_cubes_to_set(cubes)
+    cubes = transform_cubes_to_set(cubes, part)
     for _ in range(6):
-        cubes = apply_rules_to_cube(cubes)
+        cubes = apply_rules_to_cube_3d(cubes)
     return len(cubes)
 
 
 def compute_part_two(file_name: str) -> int:
-    inputs = read_input_file(file_name)
-
-    return 0
+    part = 2
+    cubes = read_input_file(file_name)
+    cubes = transform_cubes_to_set(cubes, part)
+    for _ in range(6):
+        cubes = apply_rules_to_cube_4d(cubes)
+    return len(cubes)
 
 
 if __name__ == '__main__':
