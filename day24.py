6d5dccf8701c13ac9d1951ecaee9c49fa9aa5d61
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-08-18 13:48:14 +0200

    day 24 completed

--- day24.py
+++ day24.py
@@ -38,73 +38,59 @@ def get_list_of_directions(inputs: str) -> list:
         else:
             directions.append(inputs[:1])
             inputs = inputs[1:]
-    # while inputs:
-    #     if inputs.startswith('ne'):
-    #         directions.append('ne')
-    #         inputs = inputs[2:]
-    #     elif inputs.startswith('nw'):
-    #         directions.append('nw')
-    #         inputs = inputs[2:]
-    #     elif inputs.startswith('se'):
-    #         directions.append('se')
-    #         inputs = inputs[2:]
-    #     elif inputs.startswith('sw'):
-    #         directions.append('sw')
-    #         inputs = inputs[2:]
-    #     elif inputs.startswith('e'):
-    #         directions.append('e')
-    #         inputs = inputs[1:]
-    #     else:
-    #         directions.append('w')
-    #         inputs = inputs[1:]
-
-    # print(f'{directions= }')
 
     return directions
 
 
 def get_min_max(tiles) -> (int, int, int, int):
-    # print('min-max to be implemented')
-    # coordinates = list(tiles.values())
-    # print(coordinates)
-    x_min = min(tiles.keys(), key=lambda item: item[0])[0] - 2
-    x_max = max(tiles.keys(), key=lambda item: item[0])[0] + 2
-    y_min = min(tiles.keys(), key=lambda item: item[1])[1] - 2  # to make sure we cover the edges
-    y_max = max(tiles.keys(), key=lambda item: item[1])[1] + 2
+    x_min, x_max, y_min, y_max = 0, 0, 0, 0
+    if tiles:
+        x_min = min(tiles.keys(), key=lambda item: item[0])[0] - 2
+        x_max = max(tiles.keys(), key=lambda item: item[0])[0] + 2
+        y_min = min(tiles.keys(), key=lambda item: item[1])[1] - 2  # to make sure we cover the edges
+        y_max = max(tiles.keys(), key=lambda item: item[1])[1] + 2
 
     return x_min, x_max, y_min, y_max
 
 
 def flip_flop(tiles: dict) -> dict:
     neighbours = [(2, 0), (-2, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-    x_min, x_max, y_min, y_max = get_min_max(tiles)
+    # x_min, x_max, y_min, y_max = get_min_max(tiles)
     new_tiles = dict()
-    for y in range(y_min, y_max):
-        if y % 2 == 0:
-            x_min_mod, x_max_mod = x_min // 2 * 2, x_max // 2 * 2 + 2
-        else:
-            x_min_mod, x_max_mod = x_min // 2 * 2 - 1, x_max // 2 * 2 + 1
-        for x in range(x_min_mod, x_max_mod, 2):
-            if x == 2 and y == 0:
-                pass
-            color = tiles.get((x, y), 0)
-            no_black_neighbours_tiles = 0
-            for dx, dy in neighbours:
-                if tiles.get((x + dx, y + dy), 0) == 1:
-                    no_black_neighbours_tiles += 1
+    search_area = set()  # search area consists of current black tiles + neighbours
+    for key in tiles.keys():
+        search_area.add(key)
+        for dx, dy in neighbours:
+            x, y = key
+            search_area.add((x + dx, y + dy))
+    # range (x,y):  y is even, x = -2, 0, 2, 4, etc
+    #               y is odd,  x = -1, 1, 3, 5, etc
+    # for y in range(y_min, y_max):
+    #     if y % 2 == 0:
+    #         x_min_mod, x_max_mod = x_min // 2 * 2, x_max // 2 * 2 + 2
+    #     else:
+    #         x_min_mod, x_max_mod = x_min // 2 * 2 - 1, x_max // 2 * 2 + 1
+    #     for x in range(x_min_mod, x_max_mod, 2):
+    for x, y in search_area:
+        color = tiles.get((x, y), 0)
+        no_black_neighbours_tiles = 0
+        for dx, dy in neighbours:
+            if tiles.get((x + dx, y + dy), 0) == 1:
+                no_black_neighbours_tiles += 1
 
-            if color == 1:
-                if no_black_neighbours_tiles == 0 or no_black_neighbours_tiles > 2:
-                    new_tiles.update({(x, y): 0})
-                else:
-                    new_tiles.update({(x, y): 1})
-
+        if color == 1:
+            if no_black_neighbours_tiles == 0 or no_black_neighbours_tiles > 2:
+                new_tiles.update({(x, y): 0})
             else:
-                if no_black_neighbours_tiles == 2:
-                    new_tiles.update({(x, y): 1})
-                else:
-                    new_tiles.update({(x, y): 0})
+                new_tiles.update({(x, y): 1})
 
+        else:
+            if no_black_neighbours_tiles == 2:
+                new_tiles.update({(x, y): 1})
+            else:
+                new_tiles.update({(x, y): 0})
+
+    # remove the white tiles, you don't need them
     tiles = dict()
     for key in new_tiles.keys():
         if new_tiles.get(key) == 1:
@@ -124,10 +110,6 @@ def change_tiles(inputs: list) -> dict:
         color = tiles.get((x, y), 0)
         tiles.update({(x, y): 1 - color})
 
-        # if color is None:
-        #     tiles.update({(x, y): 1})
-        # else:
-        #     tiles.update({(x, y): 1 - color})
     return tiles
 
 
